2fcf2047f2a936436219187fc6bc8a42d8bad07e
Swapnil Bilgoji <138004287+swapnilbilgoji@users.noreply.github.com.>
2025-06-23 22:56:12 +0530

    picture

--- sb-portfolio/src/components/heroSection/HeroPic.jsx
+++ sb-portfolio/src/components/heroSection/HeroPic.jsx
@@ -12,7 +12,7 @@ const HeroPic = () => {
       className=" h-full flex items-center justify-center "
     >
       <img
-        src="../../public/images/swapnil.png"
+        src="../../public/images/SwapnilSwapnil.png"
         alt="Swapnil Bilgoji"
         className="max-h-[450px] w-auto"
       />
